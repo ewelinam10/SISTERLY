b3be1befb80a1c65e1b4ab90954bf5f75dde886c
Ewelina Mysiak <Ewelina.Mysiak@assecobs.pl>
2021-05-30 09:03:09 +0200

    Recomendation section in landing page with descriptions

--- src/Components/RecomendationContainer.tsx
+++ src/Components/RecomendationContainer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import RecomendationCard from "./RecomendationCard";
-import emportret from "../images/em_portret.jpg"
-import slidebar from "../images/imitation_slide_bar.png"
+import emportret from "../images/em_portret.jpg";
+import mwportret from "../images/MW_foto.jpg";
+import kmportret from "../images/kasia.jpg";
 
 
 const RecomendationContainer = () => {
@@ -11,9 +12,9 @@ const RecomendationContainer = () => {
         <div>
             <h1>Dlaczego warto tu być?</h1>
             <div className='recomendation-card-container'>
-                <RecomendationCard img={emportret} userName='Marysia' hashtags={['przebranżawianie', 'rozwój_osobisty']} desc='Jakiś czas temu zauważyłam, że mój świat zdominowany jest przez mężczyzn. Brakowało mi kobiecej perspektywy. Sisterly daje możliwość połączenia się z dziewczynami otwartymi na wymianę doświadczeń w przyjaznej atmosferze.' />
-                <RecomendationCard img={emportret} userName='Marysia' hashtags={['przebranżawianie', 'rozwój_osobisty']} desc='Jakiś czas temu zauważyłam, że mój świat zdominowany jest przez mężczyzn. Brakowało mi kobiecej perspektywy. Sisterly daje możliwość połączenia się z dziewczynami otwartymi na wymianę doświadczeń w przyjaznej atmosferze.' />
-                <RecomendationCard img={emportret} userName='Marysia' hashtags={['przebranżawianie', 'rozwój_osobisty']} desc='Jakiś czas temu zauważyłam, że mój świat zdominowany jest przez mężczyzn. Brakowało mi kobiecej perspektywy. Sisterly daje możliwość połączenia się z dziewczynami otwartymi na wymianę doświadczeń w przyjaznej atmosferze.' />
+                <RecomendationCard img={mwportret} userName='Marysia' hashtags={['przebranżawianie', 'rozwój_osobisty']} desc='Jakiś czas temu zauważyłam, że mój świat zdominowany jest przez mężczyzn. Brakowało mi kobiecej perspektywy. Sisterly daje możliwość połączenia się z dziewczynami otwartymi na wymianę doświadczeń w przyjaznej atmosferze.' />
+                <RecomendationCard img={kmportret} userName='Kasia' hashtags={['doktoratpogodzinach', 'nieumiemwpaznokcie']} desc='Znalazłam krąg kobiet, które mnie rozumieją bez zbędnego tłumaczenia, dają mi energii do działania.W drugim kręgu, gdzie próbujemy ogarnąć bycie kobietą sukcesu, która nie umie zrobić sobie paznokci - spotkałam tam kobiety ze świetnym poczuciem humoru.'/>
+                <RecomendationCard img={emportret} userName='Ewelina' hashtags={['programowanie', 'kobietydokodu']} desc='Strona sisterly natchneła mnie do stworzenia małej grupki dziewczyn, która będzie wspólnie rozwijać wymyśloną przez nas aplikację. Strona zgrupowała mnie ze świetnymi dziewcznami i pomogła w zarządzaniu spotkaniami oraz zadaniami. ' />
             </div>
             <div className='circles'>
                 <div id="circle" />
